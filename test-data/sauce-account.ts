b5ca21a5996e01c0bf736873eafbc0f11da8adc2
nam <namnguyen.pro@live.com>
2023-09-24 21:26:02 +0700

    custom sauce data

--- test-data/sauce-account.ts
+++ test-data/sauce-account.ts
@@ -4,28 +4,7 @@ const LoginSauceDemoAccount={
 
 const StandardAccountTest={
     admin_account: {
-        email: 'standard_user',
-        password: 'secret_sauce'
-    }
-}
-
-const LockedAccountTest={
-    admin_account: {
-        email: 'locked_out_user',
-        password: 'secret_sauce'
-    }
-}
-
-const ProblemAccountTest={
-    admin_account: {
-        email: 'problem_user',
-        password: 'secret_sauce'
-    }
-}
-
-const PerformanceGlitchAccountTest={
-    admin_account: {
-        email: 'performance_glitch_user',
+        username: 'standard_user',
         password: 'secret_sauce'
     }
 }
